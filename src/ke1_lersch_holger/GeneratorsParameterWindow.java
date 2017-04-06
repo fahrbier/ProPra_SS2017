9b3a95079e1f732fafc6d22b2a7cdb0fa960e9c0
Holger Lersch <Holger@Lersch.net>
2017-04-06 05:59:10 -0400

    added Parameter Window for Simple Generators.

---
+++ src/ke1_lersch_holger/GeneratorsParameterWindow.java
@@ -0,0 +1,72 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ke1_lersch_holger;
+
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author holger
+ */
+public class GeneratorsParameterWindow {
+    
+    /* 
+    I chose a static function to have only one parameter window open. If during this project a 
+    requirement comes up to open more windows at a time, this thing needs to be refactored to 
+    add some reusability and probably not using a static function anymore - but for now it should
+    do its job.
+    */
+    
+    public static void display(){
+        
+        Stage window = new Stage();
+        
+        //-- prepare the window and set message in status bar
+        window.setTitle("Simple Generator Parameter");
+        window.setWidth(300);
+        window.setHeight(200);
+        
+        KE1_Lersch_Holger.setStatus("ready");
+        
+        //-- Parameter Height 
+        //-- todo
+        
+        //-- Parameter Width
+        //-- todo
+        
+        //-- Generate Button
+        Button generateButton = new Button("Generate");
+        generateButton.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                KE1_Lersch_Holger.setStatus("calculating");  
+                //-- implement create canvas, draw cirlce
+                //-- todo
+                
+                window.close();
+            }
+
+        });
+        
+        //-- create layout and plug all elements into it
+        VBox layout = new VBox(15);
+        layout.setAlignment(Pos.CENTER);
+        
+        layout.getChildren().add(generateButton);
+        
+        Scene sceneSimpleGeneratorsParameter = new Scene(layout);
+        
+        window.setScene(sceneSimpleGeneratorsParameter);
+        window.show();
+           
+    }
+}
