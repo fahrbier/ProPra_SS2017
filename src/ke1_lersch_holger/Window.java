f951636edb57321958c730e20db1966b3ad7749f
Holger Lersch <Holger@Lersch.net>
2017-04-14 15:51:54 -0400

    implemented saveToFile dialogue

--- src/ke1_lersch_holger/Window.java
+++ src/ke1_lersch_holger/Window.java
@@ -5,12 +5,33 @@
  */
 package ke1_lersch_holger;
 
+import java.io.File;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
+
 /**
  *
  * @author holger
  */
 public abstract class Window {
-    public void saveToFile(String fileName) {
+    public void saveToFile(String fileName, Stage stage) {
+        
+        //-- if no file name was provided, show the dialog
+        if (fileName == null) {
+            FileChooser fileChooser = new FileChooser();
+  
+              //Set extension filter
+              FileChooser.ExtensionFilter extFilter = new FileChooser.ExtensionFilter("TXT files (*.txt)", "*.txt");
+              fileChooser.getExtensionFilters().add(extFilter);
+              
+              //Show save file dialog
+              File file = fileChooser.showSaveDialog(stage);    
+        }
+        
+        
         System.out.println("Writing file " + fileName + " to file.");
+        
+        
+        
     }
 }
