3a99008c481874f42d3d100b6bd489f4bcbe637c
Holger Lersch <Holger@Lersch.net>
2017-04-14 12:02:18 -0400

    all windows are supposed to be able to save their images to file, so lets provide that functionality via an abstract class.

---
+++ src/ke1_lersch_holger/Window.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ke1_lersch_holger;
+
+/**
+ *
+ * @author holger
+ */
+public abstract class Window {
+    public void saveToFile(String fileName) {
+        System.out.println("Writing file " + fileName + " to file.");
+    }
+}
