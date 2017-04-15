d1d8a3aa0d22c5bfb31e9df7d52481059c5e0b99
Holger Lersch <Holger@Lersch.net>
2017-04-15 15:54:32 -0400

    code cleaned

--- src/ke1_lersch_holger/MainUiController.java
+++ src/ke1_lersch_holger/MainUiController.java
@@ -5,11 +5,9 @@
  */
 package ke1_lersch_holger;
 
-import java.io.IOException;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.control.Label;
 import javafx.stage.Stage;
 
@@ -23,24 +21,75 @@ import javafx.stage.Stage;
 public class MainUiController extends Window  {
     
     private Stage mainUiStage;
+    public Stage getMainUiStage() {
+        return mainUiStage;
+    }    
+    public void setMainUiStage(Stage stage) {
+        this.mainUiStage = stage;
+    }
     
     @FXML
     private SimpleGeneratorParameterController simpleGeneratorParameter;
     
     @FXML
     private  Label statusText;
+    public Label getStatusText() {
+        return statusText;
+    }
+    public void setStatusText(Label statusText) {
+        this.statusText = statusText;
+    }
+    //-- for convenience add overwritten setter which expects a simple String
+    public void setStatusText(String text) {
+        statusText.setText(text);
+    }   
+    
     
     @FXML 
     private Canvas generatedImage;
+    public Canvas getGeneratedImage(){
+        return this.generatedImage;
+    }
+    public void setGeneratedImage(Canvas image){
+        this.generatedImage = image;
+    }
+    
+  
+    private int height;
+    public int getHeight() {
+        return height;
+    }
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    
+    private int width;
+    public int getWidth() {
+        return width;
+    }
+    public void setWidth(int width) {
+        this.width = width;
+    }
+    
+    private GraphicsContext graficsContext;
+    public GraphicsContext getGraphicsContext() {
+        return this.graficsContext;
+    }
+    public void setGraphicsContext (GraphicsContext gc){
+        this.graficsContext = gc;
+    }
+
+    /*
+    * Event Handler / Actions of this Controller
+    *
+    */
+ 
     
     public void onQuit(){
         System.exit(0);
     }
-    
-    @FXML
-    public void initialize(){
-        System.out.println("App started.");
-    }
+  
     
     public void onSaveImage() {
         
@@ -53,20 +102,14 @@ public class MainUiController extends Window  {
         simpleGenerator.show(this);
     }
     
-    public void setStatusText(String text) {
-        statusText.setText(text);
-    }
+
     
-    public Canvas getGeneratedImage(){
-        return this.generatedImage;
+
+    
+    
+    public void initialize(){
+        System.out.println("App started.");
     }
 
-    public void setGeneratedImage(Canvas image){
-        this.generatedImage = image;
-    }
-    
-    public void setMainUiStage(Stage stage) {
-        this.mainUiStage = stage;
-    }
     
 }
